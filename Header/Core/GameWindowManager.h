69e1141c9229038faeade63f244fea19ecc10c11
Dip Patel <pateldeep2332@gmail.com>
2025-01-13 16:31:18 +0530

    GameWindow implementation

---
+++ Header/Core/GameWindowManager.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+using namespace sf;
+using namespace std;
+
+namespace Core {
+	class GameWindowManager {
+	private:
+		int game_window_width = 1280;
+		int game_window_height = 720;
+		string game_title = "SFML-Pong";
+
+		RenderWindow* game_window;
+
+		void createGameWindow();
+
+
+	public:
+		void initialize();
+		RenderWindow* getGameWindow();
+		bool isGameRunning();
+		void render();
+	};
+}
---
+++ Source/Core/GameWindowManager.cpp
@@ -0,0 +1,27 @@
+#include "../../Header/Core/GameWindowManager.h"
+
+namespace Core {
+
+	void GameWindowManager::initialize() {
+		game_window = new RenderWindow();
+		createGameWindow();
+	}
+
+	void GameWindowManager::createGameWindow() {
+		game_window->create(
+			VideoMode(game_window_width, game_window_height), game_title
+		);
+	}
+
+	bool GameWindowManager::isGameRunning() {
+		return game_window -> isOpen();
+	}
+
+	void GameWindowManager::render() {
+
+	}
+
+	RenderWindow* GameWindowManager::getGameWindow() {
+		return game_window;
+	}
+}
