d9e72b07b2fe6e334924ca03583df62c7da01257
ash02git <ashwinabraham2001@gmail.com>
2025-02-07 15:39:14 +0530

    Made orange background and fullscreen

--- Source/Core/GameWindowManager.cpp
+++ Source/Core/GameWindowManager.cpp
@@ -11,7 +11,8 @@ namespace Core {
 
 	void GameWindowManager::createGameWindow()
 	{
-		game_window->create(VideoMode(game_window_width, game_window_height), game_title);
+		//game_window->create(VideoMode(game_window_width, game_window_height), game_title);
+		game_window->create(sf::VideoMode::getDesktopMode(), game_title, sf::Style::Fullscreen);
 	}
 
 	bool GameWindowManager::isGameRunning()
@@ -22,6 +23,9 @@ namespace Core {
 	void GameWindowManager::render()
 	{
 		//implementation will be done later
+		game_window->clear(sf::Color(200, 50, 50, 255));
+
+		game_window->display();
 	}
 
 	RenderWindow* GameWindowManager::getGameWindow()
